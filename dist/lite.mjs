59b2e55d5116d39590eca83993d5a375ace8f3b5
xiangweiping103 <xiangweiping103@pingan.com.cn>
2019-02-03 15:10:50 +0800

    feat:切换svg.js

--- dist/lite.mjs
+++ dist/lite.mjs
@@ -1,2 +1,2 @@
-import{__assign as t}from"tslib";import r from"snapsvg";var f={fill:"#7fc8ff",stroke:"#fff",strokeWidth:1};export default function(o){var e=t({},f,o),i=r(e.width,e.height),s=e.source;return Object.keys(s).forEach(function(t){var r=s[t];i.paper.path(r).attr({fill:e.fill,stroke:e.stroke,strokeWidth:e.strokeWidth})}),i}
+import{__assign as o}from"tslib";import r from"@svgdotjs/svg.js/src/svg";console.log(r);var t={fill:"#7fc8ff",stroke:{color:"#fff",width:1}};export default function(f){var s=o({},t,f),e=r().size(s.width,s.height),i=s.source;return Object.keys(i).forEach(function(o){var r=i[o];e.path(r).fill(s.fill).stroke(s.stroke)}),e}
 //# sourceMappingURL=lite.mjs.map
--- src/lite.ts
+++ src/lite.ts
@@ -1,23 +1,22 @@
-import Snap from 'snapsvg';
+import SVG from '@svgdotjs/svg.js/src/svg';
 
 const defaultOptions = {
   fill: '#7fc8ff',
-  stroke: '#fff',
-  strokeWidth: 1,
+  stroke: {
+    color: '#fff',
+    width: 1,
+  },
 }
 
 const svgmap = (opts) => {
   const options = {...defaultOptions, ...opts}
-  const svg = Snap(options.width, options.height)
+  const svg = SVG().size(options.width, options.height)
   const shapes = options.source
   Object.keys(shapes).forEach((key) => {
     const value = shapes[key]
-    var el = svg.paper.path(value)
-    el.attr({
-        fill: options.fill,
-        stroke: options.stroke,
-        strokeWidth: options.strokeWidth,
-    });
+    svg.path(value)
+      .fill(options.fill)
+      .stroke(options.stroke)
   })
   return svg
 }
